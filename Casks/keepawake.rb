8a8159cb958f3e4af2193a7e7ed34b6345bce81d
Yashiel Sookdeo <yashiel@skyner.co.za>
2026-05-12 19:24:20 +0200

    chore: bump keepawake to v0.2.0

--- Casks/keepawake.rb
+++ Casks/keepawake.rb
@@ -1,6 +1,6 @@
 cask "keepawake" do
-  version "0.1.0"
-  sha256 "2880fa81e965302d9c881bd367220aab41916bda3bcbbfeb2ea4419df43b1268"
+  version "0.2.0"
+  sha256 "178181f82c2e410007e9fe7bdd70f41126a469208a404578b9367bb637906838"
 
   url "https://github.com/yashiels/keep-awake/releases/download/v#{version}/KeepAwake-v#{version}.dmg"
   name "KeepAwake"
