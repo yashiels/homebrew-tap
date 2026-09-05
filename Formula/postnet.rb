970070664523a3a02d5d515c988a70ec4c1c03a2
Yashiel Sookdeo <yashiel@skyner.co.za>
2026-09-05 14:10:34 +0200

    postnet: update formula for v1.0.2

--- Formula/postnet.rb
+++ Formula/postnet.rb
@@ -1,20 +1,12 @@
 class Postnet < Formula
   desc "Track PostNet parcels from the command line"
   homepage "https://github.com/yashiels/postnet-cli"
-  version "1.0.1"
-  license "MIT"
+  version "1.0.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yashiels/postnet-cli/releases/download/v#{version}/postnet-v#{version}-darwin-arm64.tar.gz"
-      sha256 "049f6560dceab45a9256602dade6c2f524b9e5c864077b488965cbba3bab4a69"
-    end
-  end
-
-  on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/yashiels/postnet-cli/releases/download/v#{version}/postnet-v#{version}-linux-x64.tar.gz"
-      sha256 "964054883eccd175bbf685142fe17906c29da531268a3198ddfcb7193ecb30e8"
+      url "https://github.com/yashiels/postnet-cli/releases/download/v1.0.2/postnet-v1.0.2-darwin-arm64.tar.gz"
+      sha256 "84f71740d0a0a281361e90b4c560024cbf05d5851d96286339e009e85ecf3589"
     end
   end
 
@@ -23,6 +15,6 @@ class Postnet < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/postnet --version")
+    system bin/"postnet", "--version"
   end
 end
