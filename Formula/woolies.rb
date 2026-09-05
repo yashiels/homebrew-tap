e2a18785ce9023a06b668490500287c9adeee841
Yashiel Sookdeo <yashiel@skyner.co.za>
2026-09-05 14:08:06 +0200

    woolies: update formula for v1.1.1

--- Formula/woolies.rb
+++ Formula/woolies.rb
@@ -1,12 +1,12 @@
 class Woolies < Formula
   desc "CLI for Woolworths Dash grocery delivery"
   homepage "https://github.com/yashiels/woolworths-cli"
-  version "1.1.0"
+  version "1.1.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yashiels/woolworths-cli/releases/download/v1.1.0/woolies-v1.1.0-darwin-arm64.tar.gz"
-      sha256 "b074de8f2236b9acebe7b004b1de481344d2f0ea525d551e4105c5a7df5b0e17"
+      url "https://github.com/yashiels/woolworths-cli/releases/download/v1.1.1/woolies-v1.1.1-darwin-arm64.tar.gz"
+      sha256 "ef869744495b7edf6f462734452c754631d058d537e4e92783a329b0965cdf96"
     end
   end
 
