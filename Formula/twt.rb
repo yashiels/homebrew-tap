ffb8df1848fe8145c0166ac2ac829eb1172b2fcf
Yashiel Sookdeo <yashiel@skyner.co.za>
2026-09-05 14:21:37 +0200

    twt: update formula for v0.2.2

--- Formula/twt.rb
+++ Formula/twt.rb
@@ -1,26 +1,26 @@
 class Twt < Formula
   desc "X/Twitter from the terminal — built on the private GraphQL API"
   homepage "https://github.com/yashiels/twitter-cli"
-  version "0.2.0"
+  version "0.2.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/yashiels/twitter-cli/releases/download/v#{version}/twitter-cli_#{version}_darwin_arm64.tar.gz"
-      sha256 "673a01d0550bdf6fd04ab02a54da88c371cb021d40b8b1f82d6ecad0831d1106"
+      sha256 "2ec8452e6d7a815214c23651736489017e71be4092e320cb2f64edb62b5aedc0"
     else
       url "https://github.com/yashiels/twitter-cli/releases/download/v#{version}/twitter-cli_#{version}_darwin_amd64.tar.gz"
-      sha256 "fb9be3f5234ae2ab379333bbe4edbb7176110319f87231b81c6b24a968ce760b"
+      sha256 "1bea0bd43a1c025db56a70f21f4eb2c314a35fc7881b34b9d6b2ff2c7682e4c2"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/yashiels/twitter-cli/releases/download/v#{version}/twitter-cli_#{version}_linux_arm64.tar.gz"
-      sha256 "ca514bf512a8e3980c4c698612c85963d577f7f73e5622ca9e7044e6b330a2c9"
+      sha256 "67ce7a89b5b8220b14d035c0dbafa62a17bff8a969a036fb2c8959bcd59090be"
     else
       url "https://github.com/yashiels/twitter-cli/releases/download/v#{version}/twitter-cli_#{version}_linux_amd64.tar.gz"
-      sha256 "9a79617aa63d64a9eec82deeb12b33dfaa10901aafe1760ca6b8dcf6d961bf35"
+      sha256 "2637532edf1447ad853565cab506a68e7ba4a71d4b8ad8ec4dc286a7e84b1bc0"
     end
   end
 
