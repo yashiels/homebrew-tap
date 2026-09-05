50d2dec9d29bf1cd49e9581e59466158e8501424
Yashiel Sookdeo <yashiel@skyner.co.za>
2026-09-05 14:07:50 +0200

    checkers60: update formula for v0.2.1

--- Formula/checkers60.rb
+++ Formula/checkers60.rb
@@ -1,12 +1,12 @@
 class Checkers60 < Formula
   desc "CLI for Checkers Sixty60 grocery delivery"
   homepage "https://github.com/yashiels/checkers60-cli"
-  version "0.2.0"
+  version "0.2.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yashiels/checkers60-cli/releases/download/v0.2.0/checkers60-v0.2.0-darwin-arm64.tar.gz"
-      sha256 "d11c4eaf520daa2bb5af346bf8b75d5a112aa8bba133ea24b1d4b69f10ba8ddf"
+      url "https://github.com/yashiels/checkers60-cli/releases/download/v0.2.1/checkers60-v0.2.1-darwin-arm64.tar.gz"
+      sha256 "723f321fa0810c939271e3ed6f2128aedf9c4abb9145a69bb3cda53cc976024f"
     end
   end
 
