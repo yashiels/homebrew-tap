17e8649bd7175f52fce592a20de2a085d8f99081
Yashiel Sookdeo <yashiel@skyner.co.za>
2026-09-05 14:03:57 +0200

    takealot: update formula for v0.6.1

--- Formula/takealot.rb
+++ Formula/takealot.rb
@@ -1,12 +1,12 @@
 class Takealot < Formula
   desc "CLI for Takealot.com"
   homepage "https://github.com/yashiels/takealot-cli"
-  version "0.6.0"
+  version "0.6.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yashiels/takealot-cli/releases/download/v0.6.0/takealot-v0.6.0-darwin-arm64.tar.gz"
-      sha256 "dbdb43e24b59c28b06a98385cf4c33295f9071bbe58a30e1d93dec8997e8e487"
+      url "https://github.com/yashiels/takealot-cli/releases/download/v0.6.1/takealot-v0.6.1-darwin-arm64.tar.gz"
+      sha256 "e92a20b3b2c89c7b86dbd0c24f6c1056a8e12ce4cde7d283bae54281c87c05d1"
     end
   end
 
