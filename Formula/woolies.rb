edeeba3ac48a5e567b4cde05829749ab8774346a
Yashiel Sookdeo <yashiel@skyner.co.za>
2026-09-06 18:57:06 +0200

    woolies: update formula for v1.1.2

--- Formula/woolies.rb
+++ Formula/woolies.rb
@@ -1,12 +1,25 @@
 class Woolies < Formula
   desc "CLI for Woolworths Dash grocery delivery"
   homepage "https://github.com/yashiels/woolworths-cli"
-  version "1.1.1"
+  version "1.1.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yashiels/woolworths-cli/releases/download/v1.1.1/woolies-v1.1.1-darwin-arm64.tar.gz"
-      sha256 "ef869744495b7edf6f462734452c754631d058d537e4e92783a329b0965cdf96"
+      url "https://github.com/yashiels/woolworths-cli/releases/download/v1.1.2/woolies-v1.1.2-darwin-arm64.tar.gz"
+      sha256 "1755def88690f4fb5dfb24349eee8c3778f4a5966493547a3dd390bd9e388158"
+    else
+      url "https://github.com/yashiels/woolworths-cli/releases/download/v1.1.2/woolies-v1.1.2-darwin-amd64.tar.gz"
+      sha256 "411b30d262aae4ffcadea4cd609df2c75fa5b0af64e3027e835b6f4222fa4955"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/yashiels/woolworths-cli/releases/download/v1.1.2/woolies-v1.1.2-linux-arm64.tar.gz"
+      sha256 "d12cc217657d0f411c265d81245d2f9a05a429ae3924d35184b33befb49e2112"
+    else
+      url "https://github.com/yashiels/woolworths-cli/releases/download/v1.1.2/woolies-v1.1.2-linux-amd64.tar.gz"
+      sha256 "edc472672b5058dd1bd2bf4c87e6df247b414becc814c8a3bb420c6772b5f61e"
     end
   end
 
