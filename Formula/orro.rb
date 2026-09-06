2331844ebb94b717c783422c1ebb8f8787969dd5
Yashiel Sookdeo <yashiel@skyner.co.za>
2026-09-06 19:13:55 +0200

    chore(orro): bump to v0.3.1 (#11)
    Closes #10

--- Formula/orro.rb
+++ Formula/orro.rb
@@ -1,26 +1,26 @@
 class Orro < Formula
   desc "Control your Tuya standing desk from the terminal"
   homepage "https://github.com/yashiels/orro-cli"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/yashiels/orro-cli/releases/download/v#{version}/orro_#{version}_darwin_arm64.tar.gz"
-      sha256 "e2586a3be411edf9792a30e5a53514efb7f7ec627c4a0004ef3deab614125db7"
+      sha256 "345d1eac2296f8de6c2ba75474816d85e3f5d3e670ba2002b9531a22f4f51beb"
     else
       url "https://github.com/yashiels/orro-cli/releases/download/v#{version}/orro_#{version}_darwin_amd64.tar.gz"
-      sha256 "bd759fba6ca2721037faf6261c71a4f9ee4f8049bf6c52d237572cab751aeac9"
+      sha256 "737a8f1347eb3c5251eb55cfa68a7597e606e06947925da82fc504f51006a39f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/yashiels/orro-cli/releases/download/v#{version}/orro_#{version}_linux_arm64.tar.gz"
-      sha256 "47f84380372a50c07b5e8812d9393d1dda814fda312cb264860863aaa7b2cf1f"
+      sha256 "f6b31a8fef4a7fe55e8c96ba94fbbdd94b548aba2b3e82278d13243a95bb9c28"
     else
       url "https://github.com/yashiels/orro-cli/releases/download/v#{version}/orro_#{version}_linux_amd64.tar.gz"
-      sha256 "6d2a2fef80634add461b76dc50e4ff86ed65f0638fdcafb3e3b179db8dd1933a"
+      sha256 "25930c4f913528655fff25159eaa34d4e9d5f746a01971e6fbce08e37e5a9679"
     end
   end
 
